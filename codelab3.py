2841e95679e9415b6f550900330547e3f03b3028
poisonslicer <103468046+poisonslicer@users.noreply.github.com>
2022-10-08 13:11:47 -0500

    Add files via upload
    Added get_average func

--- codelab3.py
+++ codelab3.py
@@ -1,14 +1,19 @@
 #First function
 #Second function
+def get_average(num_list):
+    total = 0
+    for num in num_list:
+        total += num
+    return (total/len(num_list)).__round__()
 #Third function
 #Main function
 def main():
     my_words = ['Hello', 'apple', 'one']
-    my_numbers = [5,6,7,9]
+    my_numbers = [5,6,7,9.99912]
     my_cards = ['2 of Hearts', '6 of Spades', 'Jack of Diamonds', 'Queen of clubs', 'King of Clubs']
 
     #frame_this(my_words)
-    #print(get_average(my_numbers))
+    print(get_average(my_numbers))
     #print(draw_three(my_cards))
 
 if __name__ == '__main__':
