49a798b4e2cceb5aa520217af2f956ca728c39a7
poisonslicer <103468046+poisonslicer@users.noreply.github.com>
2022-10-08 13:03:01 -0500

    Add files via upload
    Added main function calling the three others.

---
+++ codelab3.py
@@ -0,0 +1,15 @@
+#First function
+#Second function
+#Third function
+#Main function
+def main():
+    my_words = ['Hello', 'apple', 'one']
+    my_numbers = [5,6,7,9]
+    my_cards = ['2 of Hearts', '6 of Spades', 'Jack of Diamonds', 'Queen of clubs', 'King of Clubs']
+
+    #frame_this(my_words)
+    #print(get_average(my_numbers))
+    #print(draw_three(my_cards))
+
+if __name__ == '__main__':
+    main()
